c0663e09889b6b113c7211ba7bb60f36cf595b23
Nicholas R Johnson <njohnson@nicholasrjohnson>
2020-09-19 10:50:14 -0700

    Ready to make a public repo

--- Startup.cs
+++ Startup.cs
@@ -18,18 +18,15 @@ namespace Vesuvius
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "apps/vesuvius/ClientApp/dist";//"apps/vesuvius/ClientApp/dist";
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -39,8 +36,6 @@ namespace Vesuvius
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
             app.UseStaticFiles();
@@ -67,8 +62,6 @@ namespace Vesuvius
              
             app.UseSpa(spa =>
             {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
 
                 spa.Options.SourcePath = "ClientApp";
 
